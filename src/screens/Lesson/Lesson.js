ef07d59f4b85d162f91c1a5625e07209321f7de5
Mohammad Kashif Sulaiman <m.kqagraphix@gmail.com>
2018-10-20 16:56:02 +0500

    Next and back functionality

--- src/screens/Lesson/Lesson.js
+++ src/screens/Lesson/Lesson.js
@@ -16,6 +16,25 @@ class Lesson extends Component {
         }
     }
 
+    changePage(next) {
+        const { pageNo } = this.state;
+        const newPageNo = pageNo + (next ? 1 : -1)
+        const lessonNo = +this.props.match.params.id;
+        const lesson = LESSON_DATA[lessonNo].pages[newPageNo];
+
+        if(lesson) {
+            this.setState({
+                title: lesson.title.text,
+                paragraph: lesson.paragraph.text,
+                pageNo: newPageNo
+            })
+        } else {
+            if(lessonNo || next) {
+                this.props.history.push(`/lesson/${lessonNo + (next ? 1 : -1)}`)
+            }
+        }
+    }
+
     render() {
         const { currentLesson, pageNo, title, paragraph } = this.state;
         const page = currentLesson.pages[pageNo];
@@ -26,8 +45,8 @@ class Lesson extends Component {
                 <h2>Page { page.id }</h2>
                 <h3> { title } </h3>
                 <p> { paragraph } </p>
-                <Button color="primary">Back</Button>
-                <Button color="primary">Next</Button>
+                <Button onClick={ this.changePage.bind(this, false) } color="primary">Back</Button>
+                <Button onClick={ this.changePage.bind(this, true) } color="primary">Next</Button>
             </div>
         );
     }
