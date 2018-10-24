00dc85859a27ab2548629179307477af595969f2
Mohammad Kashif Sulaiman <m.kqagraphix@gmail.com>
2018-10-24 18:12:01 +0500

    fix next and previous functionality

--- src/screens/Lesson/Lesson.js
+++ src/screens/Lesson/Lesson.js
@@ -4,7 +4,6 @@ import Button from '@material-ui/core/Button';
 import LESSON_DATA from '../../constant/Lesson_data';
 
 class Lesson extends Component {
-
     constructor(props) {
         super();
 
@@ -34,7 +33,28 @@ class Lesson extends Component {
     }
 
     changePage(next) {
-        const { pageNo, lessonNo } = this.state;
+        let { lessonNo, pageNo } = this.state;
+
+        // if back button pressed and should jump to previous lesson final page
+        if (!next && pageNo === 0) {
+            const prevLessonNo = lessonNo - 1;
+            const prevLesson = LESSON_DATA[prevLessonNo];
+            if(prevLesson){
+                pageNo = prevLesson.pages.length - 1;
+                this.setState({
+                    showingFrench: true,
+                    pageNo,
+                    currentLesson: prevLesson,
+                    html: prevLesson.pages[pageNo].html,
+                    htmlToRendered: prevLesson.pages[pageNo].html,
+                    foreignHtml: prevLesson.pages[pageNo].foreignHtml,
+                    lessonNo: prevLessonNo
+                })
+                this.props.history.push(`/lesson/${prevLessonNo}`)
+            }
+            return;
+        }
+
         const newPageNo = pageNo + (next ? 1 : -1)
         const lesson = LESSON_DATA[lessonNo].pages[newPageNo];
 
@@ -45,9 +65,22 @@ class Lesson extends Component {
                 foreignHtml: lesson.foreignHtml,
                 pageNo: newPageNo
             })
-        } else {
+        }
+
+        // if pages are over and should jump to next lesson
+        else {
             if (lessonNo || next) {
-                this.props.history.push(`/lesson/${lessonNo + (next ? 1 : -1)}`)
+                lessonNo = lessonNo + 1;
+                this.setState({
+                    showingFrench: true,
+                    lessonNo,
+                    currentLesson: LESSON_DATA[lessonNo],
+                    html: LESSON_DATA[lessonNo].pages[0].html,
+                    htmlToRendered: LESSON_DATA[lessonNo].pages[0].html,
+                    foreignHtml: LESSON_DATA[lessonNo].pages[0].foreignHtml,
+                    pageNo: 0
+                })
+                this.props.history.push(`/lesson/${lessonNo}`)
             }
         }
     }
